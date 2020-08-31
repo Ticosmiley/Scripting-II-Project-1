5c66cde5148abeab385b5a0ebf4c00018a25d1a7
Henry Hazlett <ticotoad@gmail.com>
2020-08-31 18:15:56 -0500

    Add model and animations

---
+++ Assets/Scripts/PlayerCharacterAnimator.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerCharacterAnimator : MonoBehaviour
+{
+    [SerializeField] ThirdPersonMovement _thirdPersonMovement = null;
+
+    const string IdleState = "Idle";
+    const string RunState = "Run";
+    const string JumpState = "Jumping";
+    const string FallState = "Falling";
+    const string LandState = "Landing";
+
+    Animator _animator = null;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _thirdPersonMovement.Idle += OnIdle;
+        _thirdPersonMovement.StartRunning += OnStartRunning;
+        _thirdPersonMovement.Jump += OnJump;
+        _thirdPersonMovement.StartFalling += OnStartFalling;
+        _thirdPersonMovement.Land += OnLand;
+    }
+
+    private void OnDisable()
+    {
+        _thirdPersonMovement.Idle -= OnIdle;
+        _thirdPersonMovement.StartRunning -= OnStartRunning;
+        _thirdPersonMovement.Jump -= OnJump;
+        _thirdPersonMovement.StartFalling -= OnStartFalling;
+        _thirdPersonMovement.Land -= OnLand;
+    }
+
+    public void OnIdle()
+    {
+        _animator.CrossFadeInFixedTime(IdleState, .2f);
+    }
+
+    private void OnStartRunning()
+    {
+        _animator.CrossFadeInFixedTime(RunState, .2f);
+    }
+
+    private void OnJump()
+    {
+        _animator.CrossFadeInFixedTime(JumpState, .2f);
+    }
+
+    private void OnStartFalling()
+    {
+        _animator.CrossFadeInFixedTime(FallState, .2f);
+    }
+
+    private void OnLand()
+    {
+        _animator.CrossFadeInFixedTime(LandState, .2f);
+    }
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,31 +1,104 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+    public event Action Idle = delegate { };
+    public event Action StartRunning = delegate { };
+    public event Action Jump = delegate { };
+    public event Action StartFalling = delegate { };
+    public event Action Land = delegate { };
+
+
     public CharacterController controller;
     public Transform cam;
 
     public float speed = 6f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 1.0f;
+    private Vector3 yVelocity;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    bool _isRunning = false;
+    bool _isFalling = false;
+
+    private void Start()
+    {
+        Idle?.Invoke();
+    }
 
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 moveDir = Vector3.zero;
 
         if (direction.magnitude >= 0.1f)
         {
+            CheckIfStartedMoving();
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
         }
+        else
+        {
+            CheckIfStoppedMoving();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            Jump?.Invoke();
+            _isRunning = false;
+            yVelocity.y = 0;
+            yVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+
+        if (!controller.isGrounded)
+        {
+            if (yVelocity.y < 0 && !_isFalling)
+            {
+                StartFalling?.Invoke();
+                _isFalling = true;
+            }
+        }
+
+        if (_isFalling && controller.isGrounded)
+        {
+            CheckIfStartedMoving();
+            if (!_isRunning)
+            {
+                Land?.Invoke();
+            }
+            _isFalling = false;
+        }
+
+        yVelocity.y += gravity * Time.deltaTime;
+        moveDir += yVelocity;
+        controller.Move(moveDir * Time.deltaTime);
+    }
+
+    private void CheckIfStartedMoving()
+    {
+        if (!_isRunning && controller.isGrounded && !_isFalling)
+        {
+            StartRunning?.Invoke();
+            _isRunning = true;
+        }
+    }
+
+    private void CheckIfStoppedMoving()
+    {
+        if (_isRunning)
+        {
+            Idle?.Invoke();
+        }
+        _isRunning = false;
     }
 }
